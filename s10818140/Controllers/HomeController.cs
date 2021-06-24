6f3fc94c08822d59abe8165a51aa6e57460acf0c
劉諺鴻 <davidalan0826@gmail.com>
2021-06-24 19:54:52 +0800

    輸入數字 可以查詢

--- s10818140/Controllers/HomeController.cs
+++ s10818140/Controllers/HomeController.cs
@@ -36,6 +36,17 @@ namespace s10818140.Controllers
         [HttpPost]
         public ActionResult Week(int a)
         {
+            var b = "";
+            if (a == 1) ViewBag.k = "星期一";
+            if (a == 2) ViewBag.k = "星期二";
+            if (a == 3) ViewBag.k = "星期三";
+            if (a == 4) ViewBag.k = "星期四";
+            if (a == 5) ViewBag.k = "星期五";
+            if (a == 6) ViewBag.k = "星期六";
+            if (a == 7) ViewBag.k = "星期日";
+
+
+
 
             return View();
         }
